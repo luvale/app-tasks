7f8508b6cd51d130b65717856f926af56fcfff32
unknown <valeriae200@hotmail.com>
2024-05-15 17:35:41 -0500

    create components and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'tasks', component: TasksComponent},
+];
